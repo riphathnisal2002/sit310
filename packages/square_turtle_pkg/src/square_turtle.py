e333e1654b4405bc5325fbc150b286927ebdb845
nisal <riphathnisal2@gmail.com>
2025-03-28 21:10:27 +1100

    second entry

--- packages/square_turtle_pkg/src/square_turtle.py
+++ packages/square_turtle_pkg/src/square_turtle.py
@@ -1,42 +1,36 @@
 #!/usr/bin/env python3
 
 # Import Dependencies
-import rospy 
-from geometry_msgs.msg import Twist 
-import time 
+import rospy
+from geometry_msgs.msg import Twist
+import time
 
-def move_turtle_square(): 
+def move_turtle_square():
     rospy.init_node('turtlesim_square_node', anonymous=True)
-    
+
     # Init publisher
-    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10) 
+    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     rospy.loginfo("Turtles are great at drawing squares!")
 
-    ########## YOUR CODE GOES HERE ##########
+    # Create a Twist message for controlling the turtle
+    cmd_vel_msg = Twist()
+
     while not rospy.is_shutdown():
-        
-        rate = rospy.Rate(1)  # 1 Hz, adjust as needed
-        
-        # Create a Twist message for moving forward 
-        cmd_vel_msg = Twist() 
+        # Move forward for some time
         cmd_vel_msg.linear.x = 2.0  # Linear velocity
-        velocity_publisher.publish(cmd_vel_msg) # Publish!
+        cmd_vel_msg.angular.z = 0.0  # No angular velocity (straight)
+        velocity_publisher.publish(cmd_vel_msg)  # Publish the forward command
+        rospy.sleep(2)  # Adjust this time based on how far the turtle should move
 
-        rate.sleep()
+        # Turn 90 degrees
+        cmd_vel_msg.linear.x = 0.0  # Stop moving forward
+        cmd_vel_msg.angular.z = 1.57  # Set angular velocity to 90 degrees per second (1.57 radians per second)
+        velocity_publisher.publish(cmd_vel_msg)  # Publish the turn command
+        rospy.sleep(1.5)  # Time to turn 90 degrees (adjust based on angular speed)
 
-        # Create a Twist message for moving backward 
-        cmd_vel_msg = Twist()
-        cmd_vel_msg.linear.x = -2.0  # Linear velocity
-        velocity_publisher.publish(cmd_vel_msg) # Publish!
-
-        rate.sleep()
-
-        ###########################################
-
-if __name__ == '__main__': 
-
-    try: 
-        move_turtle_square() 
-    except rospy.ROSInterruptException: 
+if __name__ == '__main__':
+    try:
+        move_turtle_square()
+    except rospy.ROSInterruptException:
         pass
-        
+
