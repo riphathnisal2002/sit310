01ed59eac4435312529e17114f4131d1894842f6
nisal <riphathnisal2@gmail.com>
2025-05-12 14:45:14 +1000

    initail commit

--- packages/target_follower/src/target_follower.py
+++ packages/target_follower/src/target_follower.py
@@ -8,15 +8,26 @@ class Target_Follower:
     def __init__(self):
         rospy.init_node('target_follower_node', anonymous=True)
         rospy.on_shutdown(self.clean_shutdown)
-        self.cmd_vel_pub = rospy.Publisher('/birdie/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
-        rospy.Subscriber('/birdie/apriltag_detector_node/detections', AprilTagDetectionArray, self.tag_callback, queue_size=1)
 
-        # Control parameters
+        self.cmd_vel_pub = rospy.Publisher(
+            '/birdie/car_cmd_switch_node/cmd',
+            Twist2DStamped,
+            queue_size=1
+        )
+
+        rospy.Subscriber(
+            '/birdie/apriltag_detector_node/detections',
+            AprilTagDetectionArray,
+            self.tag_callback,
+            queue_size=1
+        )
+
+        # Control parameters (tuned for closer stop and more lenient alignment)
         self.rotation_speed = 3.0
         self.forward_speed = 0.2
-        self.rotation_threshold = 0.035  # x-axis threshold to decide rotation
-        self.desired_distance = 0.4      # meters
-        self.distance_tolerance = 0.05   # acceptable range around desired distance
+        self.rotation_threshold = 0.07       # Allow small misalignment
+        self.desired_distance = 0.2          # Target stopping distance (meters)
+        self.distance_tolerance = 0.02       # Acceptable +/- range from desired
 
         rospy.spin()
 
@@ -45,10 +56,11 @@ class Target_Follower:
             self.cmd_vel_pub.publish(cmd_msg)
             return
 
-        # --- Tag detected ---
+        # Find the closest tag
         closest_tag = min(detections, key=lambda tag: tag.transform.translation.z)
         x = closest_tag.transform.translation.x
         z = closest_tag.transform.translation.z
+
         rospy.loginfo("Tag detected. x: %.3f, z: %.3f", x, z)
 
         aligned = False
@@ -61,16 +73,16 @@ class Target_Follower:
             cmd_msg.omega = abs(self.rotation_speed)   # Turn left
             rospy.loginfo("Rotating left")
         else:
-            cmd_msg.omega = 0.0  # Aligned
+            cmd_msg.omega = 0.0
             aligned = True
             rospy.loginfo("Tag aligned")
 
-        # --- Forward movement only when aligned ---
+        # --- Forward motion only if aligned and too far ---
         if aligned and z > self.desired_distance + self.distance_tolerance:
-            cmd_msg.v = self.forward_speed  # Move forward
+            cmd_msg.v = self.forward_speed
             rospy.loginfo("Moving forward")
         else:
-            cmd_msg.v = 0.0  # Stop if not aligned or already close enough
+            cmd_msg.v = 0.0
             if aligned:
                 rospy.loginfo("At optimal distance or not far enough - stopping")
             else:
@@ -82,4 +94,6 @@ class Target_Follower:
 if __name__ == '__main__':
     try:
         target_follower = Target_Follower()
+    except rospy.ROSInterruptException:
+        pass
 
