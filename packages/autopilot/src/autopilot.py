6be91596679d60c75137180a2f53485454aa2829
nisal <riphathnisal2@gmail.com>
2025-05-21 16:40:19 +1000

    initial commiy

--- packages/autopilot/src/autopilot.py
+++ packages/autopilot/src/autopilot.py
@@ -4,47 +4,36 @@ import rospy
 from duckietown_msgs.msg import Twist2DStamped
 from duckietown_msgs.msg import FSMState
 from duckietown_msgs.msg import AprilTagDetectionArray
-import time
 
 class Autopilot:
     def __init__(self):
-        
-        #Initialize ROS node
+        # Initialize ROS node
         rospy.init_node('autopilot_node', anonymous=True)
 
         self.robot_state = "LANE_FOLLOWING"
 
-        # When shutdown signal is received, we run clean_shutdown function
+        # Shutdown callback
         rospy.on_shutdown(self.clean_shutdown)
-        
+
         ###### Init Pub/Subs. REMEMBER TO REPLACE "akandb" WITH YOUR ROBOT'S NAME #####
         self.cmd_vel_pub = rospy.Publisher('/birdie/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
         self.state_pub = rospy.Publisher('/birdie/fsm_node/mode', FSMState, queue_size=1)
         rospy.Subscriber('/birdie/apriltag_detector_node/detections', AprilTagDetectionArray, self.tag_callback, queue_size=1)
         ################################################################
 
-        rospy.spin() # Spin forever but listen to message callbacks
+        rospy.spin()  # Listen for messages indefinitely
 
-        self.last_stop_time = 0
-        self.cooldown_duration = 5.0  # Seconds between stops
-        self.stop_duration = 3.0
-
-
-    
-    
     # Apriltag Detection Callback
     def tag_callback(self, msg):
         if self.robot_state != "LANE_FOLLOWING":
             return
-        
+
         self.move_robot(msg.detections)
- 
-    # Stop Robot before node has shut down. This ensures the robot keep moving with the latest velocity command
+
     def clean_shutdown(self):
         rospy.loginfo("System shutting down. Stopping robot...")
         self.stop_robot()
 
-    # Sends zero velocity to stop the robot
     def stop_robot(self):
         cmd_msg = Twist2DStamped()
         cmd_msg.header.stamp = rospy.Time.now()
@@ -63,23 +52,42 @@ class Autopilot:
         if len(detections) == 0:
             return
 
-        current_time = time.time()
-        time_since_last_stop = current_time - self.last_stop_time
+        # Switch to joystick control to override lane following
+        self.set_state("NORMAL_JOYSTICK_CONTROL")
 
-        if time_since_last_stop < self.cooldown_duration:
-            # Still in cooldown, ignore tag
-            return
+        # Create velocity command message
+        cmd_msg = Twist2DStamped()
+        cmd_msg.header.stamp = rospy.Time.now()
 
-        rospy.loginfo("Stop sign detected. Stopping robot for %.1f seconds.", self.stop_duration)
+        # React to first recognized tag only
+        for detection in detections:
+            tag_id = detection.id[0]
+            rospy.loginfo(f"Detected AprilTag ID: {tag_id}")
 
-        # Stop the robot
-        self.set_state("NORMAL_JOYSTICK_CONTROL")  # Stops lane following
-        self.stop_robot()
-        rospy.sleep(self.stop_duration)
+            if tag_id == 1:
+                # Move forward
+                cmd_msg.v = 0.3
+                cmd_msg.omega = 0.0
+            elif tag_id == 2:
+                # Turn left
+                cmd_msg.v = 0.0
+                cmd_msg.omega = 1.0
+            elif tag_id == 3:
+                # Turn right
+                cmd_msg.v = 0.0
+                cmd_msg.omega = -1.0
+            else:
+                rospy.loginfo("Unrecognized tag ID. Ignoring.")
+                return
 
-        # Resume lane following
+            self.cmd_vel_pub.publish(cmd_msg)
+            rospy.sleep(1.0)  # Move for 1 second
+            self.stop_robot()
+            rospy.sleep(0.5)
+            break  # Only act on one detection at a time
+
+        # Return to lane following mode
         self.set_state("LANE_FOLLOWING")
-        self.last_stop_time = time.time()
 
 if __name__ == '__main__':
     try:
