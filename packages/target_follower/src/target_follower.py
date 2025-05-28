ae3db0331198e28ff055a0ba97742cf2c9db38b4
nisal <riphathnisal2@gmail.com>
2025-05-28 12:16:23 +1000

    initial commit

--- packages/target_follower/src/target_follower.py
+++ packages/target_follower/src/target_follower.py
@@ -1,99 +1,60 @@
-#!/usr/bin/env python3
 import rospy
-from duckietown_msgs.msg import Twist2DStamped
-from duckietown_msgs.msg import AprilTagDetectionArray
+from duckietown_msgs.msg import Twist2DStamped, BoolStamped
+from std_msgs.msg import Header
+from threading import Timer
 
-class Target_Follower:
-
+class StopSignBehavior:
     def __init__(self):
-        rospy.init_node('target_follower_node', anonymous=True)
-        rospy.on_shutdown(self.clean_shutdown)
+        rospy.init_node('stop_sign_behavior_node')
 
-        self.cmd_vel_pub = rospy.Publisher(
-            '/birdie/car_cmd_switch_node/cmd',
-            Twist2DStamped,
-            queue_size=1
-        )
+        self.car_cmd_pub = rospy.Publisher("/birdie/car_cmd", Twist2DStamped, queue_size=1)
+        self.stop_sign_sub = rospy.Subscriber("/birdie/stop_sign_detected", BoolStamped, self.stop_sign_cb)
+        self.lane_follow_sub = rospy.Subscriber("/birdie/lane_controller/car_cmd", Twist2DStamped, self.lane_follow_cb)
 
-        rospy.Subscriber(
-            '/birdie/apriltag_detector_node/detections',
-            AprilTagDetectionArray,
-            self.tag_callback,
-            queue_size=1
-        )
+        self.state = "LANE_FOLLOWING"
+        self.ignore_stop_signs = False
+        self.ignore_duration = 4  # seconds
+        self.wait_duration = 3    # stop duration
 
-        # Control parameters (tuned for closer stop and more lenient alignment)
-        self.rotation_speed = 3.0
-        self.forward_speed = 0.2
-        self.rotation_threshold = 0.07       # Allow small misalignment
-        self.desired_distance = 0.2          # Target stopping distance (meters)
-        self.distance_tolerance = 0.02       # Acceptable +/- range from desired
+    def stop_sign_cb(self, msg):
+        if self.ignore_stop_signs:
+            return  # Ignore tag callbacks during this window
+        if msg.data and self.state == "LANE_FOLLOWING":
+            rospy.loginfo("Stop Sign Detected!")
+            self.state = "STOPPING"
 
+    def lane_follow_cb(self, msg):
+        if self.state == "LANE_FOLLOWING":
+            self.car_cmd_pub.publish(msg)
+        elif self.state == "STOPPING":
+            self.publish_stop()
+            rospy.Timer(rospy.Duration(self.wait_duration), self.resume_after_stop, oneshot=True)
+            self.state = "WAITING"
+        elif self.state == "WAITING":
+            self.publish_stop()
+        elif self.state == "IGNORING_TAGS":
+            self.car_cmd_pub.publish(msg)
+
+    def publish_stop(self):
+        stop_msg = Twist2DStamped()
+        stop_msg.header = Header()
+        stop_msg.v = 0.0
+        stop_msg.omega = 0.0
+        self.car_cmd_pub.publish(stop_msg)
+
+    def resume_after_stop(self, event):
+        rospy.loginfo("Stop complete. Resuming lane following and ignoring stop signs for a while.")
+        self.ignore_stop_signs = True
+        self.state = "IGNORING_TAGS"
+        rospy.Timer(rospy.Duration(self.ignore_duration), self.stop_ignoring_tags, oneshot=True)
+
+    def stop_ignoring_tags(self, event):
+        rospy.loginfo("Done ignoring stop signs.")
+        self.ignore_stop_signs = False
+        self.state = "LANE_FOLLOWING"
+
+    def run(self):
         rospy.spin()
 
-    def tag_callback(self, msg):
-        self.move_robot(msg.detections)
-
-    def clean_shutdown(self):
-        rospy.loginfo("Shutting down. Stopping robot...")
-        self.stop_robot()
-
-    def stop_robot(self):
-        cmd_msg = Twist2DStamped()
-        cmd_msg.header.stamp = rospy.Time.now()
-        cmd_msg.v = 0.0
-        cmd_msg.omega = 0.0
-        self.cmd_vel_pub.publish(cmd_msg)
-
-    def move_robot(self, detections):
-        cmd_msg = Twist2DStamped()
-        cmd_msg.header.stamp = rospy.Time.now()
-
-        if len(detections) == 0:
-            rospy.loginfo("No tag detected. Staying stationary.")
-            cmd_msg.v = 0.0
-            cmd_msg.omega = 0.0
-            self.cmd_vel_pub.publish(cmd_msg)
-            return
-
-        # Find the closest tag
-        closest_tag = min(detections, key=lambda tag: tag.transform.translation.z)
-        x = closest_tag.transform.translation.x
-        z = closest_tag.transform.translation.z
-
-        rospy.loginfo("Tag detected. x: %.3f, z: %.3f", x, z)
-
-        aligned = False
-
-        # --- Rotation control ---
-        if x > self.rotation_threshold:
-            cmd_msg.omega = -abs(self.rotation_speed)  # Turn right
-            rospy.loginfo("Rotating right")
-        elif x < -self.rotation_threshold:
-            cmd_msg.omega = abs(self.rotation_speed)   # Turn left
-            rospy.loginfo("Rotating left")
-        else:
-            cmd_msg.omega = 0.0
-            aligned = True
-            rospy.loginfo("Tag aligned")
-
-        # --- Forward motion only if aligned and too far ---
-        if aligned and z > self.desired_distance + self.distance_tolerance:
-            cmd_msg.v = self.forward_speed
-            rospy.loginfo("Moving forward")
-        else:
-            cmd_msg.v = 0.0
-            if aligned:
-                rospy.loginfo("At optimal distance or not far enough - stopping")
-            else:
-                rospy.loginfo("Not aligned - holding position")
-
-        self.cmd_vel_pub.publish(cmd_msg)
-
-
 if __name__ == '__main__':
-    try:
-        target_follower = Target_Follower()
-    except rospy.ROSInterruptException:
-        pass
-
+    StopSignBehavior().run()
