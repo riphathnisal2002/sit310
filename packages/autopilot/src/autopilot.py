69123bb0f57471ea7590b22a59df01dbd4953373
nisal <riphathnisal2@gmail.com>
2025-05-28 13:53:27 +1000

    mk1

--- packages/autopilot/src/autopilot.py
+++ packages/autopilot/src/autopilot.py
@@ -4,21 +4,21 @@ import rospy
 from duckietown_msgs.msg import Twist2DStamped
 from duckietown_msgs.msg import FSMState
 from duckietown_msgs.msg import AprilTagDetectionArray
-import time
 
 class Autopilot:
     def __init__(self):
         rospy.init_node('autopilot_node', anonymous=True)
-
         self.robot_state = "LANE_FOLLOWING"
-        self.last_stop_time = None
-        self.stop_cooldown = 5.0  # Seconds
+        self.tag_cooldown = rospy.Duration(5.0)  # 5-second cooldown
+        self.last_tag_time = rospy.Time(0)
 
         rospy.on_shutdown(self.clean_shutdown)
 
+        # Publishers
         self.cmd_vel_pub = rospy.Publisher('/birdie/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
         self.state_pub = rospy.Publisher('/birdie/fsm_node/mode', FSMState, queue_size=1)
 
+        # Subscribers
         rospy.Subscriber('/birdie/apriltag_detector_node/detections', AprilTagDetectionArray, self.tag_callback, queue_size=1)
 
         rospy.spin()
@@ -27,24 +27,26 @@ class Autopilot:
         if self.robot_state != "LANE_FOLLOWING":
             return
 
-        current_time = time.time()
+        current_time = rospy.Time.now()
 
-        # Only react to stop sign (AprilTag ID 31)
-        stop_sign_detected = any(tag.id == 31 for tag in msg.detections)
+        if len(msg.detections) > 0 and (current_time - self.last_tag_time) > self.tag_cooldown:
+            self.last_tag_time = current_time
+            rospy.loginfo("AprilTag detected. Stopping for 3 seconds.")
+            self.stop_and_wait()
 
-        if not stop_sign_detected:
-            return
-
-        # Avoid multiple stops in short time
-        if self.last_stop_time and (current_time - self.last_stop_time < self.stop_cooldown):
-            return
-
-        self.last_stop_time = current_time
-        self.perform_stop_and_resume()
-
-    def clean_shutdown(self):
-        rospy.loginfo("System shutting down. Stopping robot...")
+    def stop_and_wait(self):
+        # Stop the robot
         self.stop_robot()
+
+        # Switch FSM mode to something that doesn't conflict with lane following
+        self.set_state("NORMAL_JOYSTICK_CONTROL")
+
+        # Wait for 3 seconds
+        rospy.sleep(3.0)
+
+        # Resume lane following
+        self.set_state("LANE_FOLLOWING")
+        rospy.loginfo("Resuming lane following.")
 
     def stop_robot(self):
         cmd_msg = Twist2DStamped()
@@ -57,23 +59,15 @@ class Autopilot:
         self.robot_state = state
         state_msg = FSMState()
         state_msg.header.stamp = rospy.Time.now()
-        state_msg.state = state
+        state_msg.state = self.robot_state
         self.state_pub.publish(state_msg)
 
-    def perform_stop_and_resume(self):
-        rospy.loginfo("Stop sign (ID 31) detected. Stopping for 3 seconds.")
-
+    def clean_shutdown(self):
+        rospy.loginfo("System shutting down. Stopping robot...")
         self.stop_robot()
-        self.set_state("NORMAL_JOYSTICK_CONTROL")
-
-        rospy.sleep(3.0)
-
-        self.set_state("LANE_FOLLOWING")
-        rospy.loginfo("Resuming lane following.")
 
 if __name__ == '__main__':
     try:
         autopilot_instance = Autopilot()
     except rospy.ROSInterruptException:
         pass
-
